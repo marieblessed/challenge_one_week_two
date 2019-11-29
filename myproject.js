5ae0ba375e1d2ba14f4af090eb1cdfd5487c56f5
marieblessed <mmusimenta@gmail.com>
2019-11-29 18:40:02 +0300

    javascript if condition

---
+++ myproject.js
@@ -0,0 +1,16 @@
+var  yearofbirth = 1996;
+var currentyear = 2019;
+var   age = (currentyear-yearofbirth);
+if (age<18){
+
+
+console.log('user minor');
+}
+else if(age<18<36){
+
+    console.log('user is a youth');
+
+}
+else{
+    console.log('user is an elder');
+}
